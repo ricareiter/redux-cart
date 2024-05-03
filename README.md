b88143d596eebeb4b350b3f509b6b291f94aa3d0
Ricardo <ricardoreiterr@gmail.com>
2024-05-03 14:46:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Redux-Cart
+
+Shopping cart with redux toolkit.
+
+<p>
+    <img src="public/project.png" >
+</p>
+
+## Built With
+
+- React
+- Redux Toolkit
+
+## Author
+
+**Ricardo Reiter**
+
+- GitHub: [@ricareiter](https://github.com/ricareiter)
+- Linkedin [Ricardo Reiter](https://www.linkedin.com/in/ricardoreiter/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/ricareiter/tip-calculator/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,65 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "react-useReducer-cart-project";
+
+export const getCartItems = createAsyncThunk("cart/getCartItems", async () => {
+  const { data } = await axios.get(url);
+  return data;
+});
+
+const initialState = {
+  cartItems: [],
+  amount: 4,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotals: (state) => {
+      let total = 0;
+      let amount = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getCartItems.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getCartItems.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    });
+    builder.addCase(getCartItems.rejected, (state, action) => {
+      state.isLoading = false;
+    });
+  },
+});
+
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
+export default cartSlice.reducer;
